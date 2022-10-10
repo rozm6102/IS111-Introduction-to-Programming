09139032a27b278d1cc3c25880ddd43da48ca6a7
Rozalin Mohanty <115343119+rozm6102@users.noreply.github.com>
2022-10-10 21:42:25 +0800

    Add files via upload

---
+++ Lab Test 1 (AY2018-19 T1)/q2.py
@@ -0,0 +1,35 @@
+#
+# Name: 
+# Email ID: 
+#
+def represent_numbers (start, end):
+    # write your answer between #startcode and #endcode
+    #startcode
+    return ''
+    #endcode
+
+
+print('Test 1')
+print('Expected:-#--#---#----#-----')
+print('Actual  :' + represent_numbers(1, 5))
+print()
+
+print('Test 2')
+print('Expected:---#----#-----')
+print('Actual  :' + represent_numbers(3, 5))
+print()
+
+print('Test 3')
+print('Expected:-')
+print('Actual  :' + represent_numbers(1, 1))
+print()
+
+print('Test 4')
+print('Expected:[]')
+print('Actual  :[' + represent_numbers(4, 1) + ']')
+print()
+
+print('Test 5')
+print('Expected:----#---#--#-##-')
+print('Actual  :' + represent_numbers(-4, 1))
+print()
---
+++ Lab Test 1 (AY2018-19 T1)/q3.py
@@ -0,0 +1,30 @@
+#
+# Name: 
+# Email ID: 
+#
+def mask_out(sentence, banned, substitutes):
+    # write your answer between #start and #end
+    #start
+    return ''
+    #end
+
+
+print('Test 1')
+print('Expected:abcd#')
+print('Actual  :' + mask_out('abcde', 'e', '#'))
+print()
+
+print('Test 2')
+print('Expected:#$solute')
+print('Actual  :' + mask_out('absolute', 'ab', '#$'))
+print()
+
+print('Test 3')
+print('Expected:121hon')
+print('Actual  :' + mask_out('python', 'pyt', '12'))
+print()
+
+
+
+
+
